d8832e48d2b4462765f276ea71f2226952407af5
kosumi <chankocyo@gmail.com>
2025-05-10 00:46:12 -0400

    More general, applies to any symbol

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,29 @@ use thirtyfour::prelude::*;
 async fn main() -> WebDriverResult<()> {
      let mut caps = DesiredCapabilities::chrome();
      caps.set_binary("/nix/store/kfah02kcpl8wgf85wchhcmjdqamm27b6-google-chrome-135.0.7049.52/bin/google-chrome-stable")?;
-     caps.add_arg("--headless=new")?;
+     // caps.add_arg("--headless=new")?;
 
      let driver = WebDriver::new("http://localhost:40213", caps).await?;
 
      // Navigate to https://wikipedia.org.
-     driver.goto("https://www.tradingview.com/symbols/XAUUSD").await?;
+     driver.goto("https://www.tradingview.com/symbols/ETHUSD?exchange=CRYPTO").await?;
      // Locate the element using CSS selector
-     let parent_element = driver.find(By::Css("div.js-symbol-header-ticker[data-symbol='OANDA:XAUUSD']")).await?;
+     let parent_element = driver.query(By::ClassName("js-symbol-header-ticker")).first().await?;
 
     let child_element = parent_element.find(By::ClassName("js-symbol-last")).await?;
-
-     let span_element = child_element.find(By::Tag("span")).await?;
-
-    // Retrieve the text content
-    let text = span_element.text().await?;
-    let number: f64 = text
+    loop {
+       use strip_tags::strip_tags; 
+    let number = child_element.inner_html().await?;
+    let number = strip_tags(&number);
+    let number: f64 = number 
     .replace(",", "") // Remove commas if present
     .parse().unwrap();
 
-     println!("Extracted number: {}", number);
-     //
-     // // Look for header to implicitly wait for the page to load.
-     // driver.find(By::ClassName("firstHeading")).await?;
-     // assert_eq!(driver.title().await?, "Selenium - Wikipedia");
-    
+     println!("Extracted number: {:?}", number);
+    use tokio::time::Duration;
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    }
+
      // Always explicitly close the browser.
      driver.quit().await?;
 
